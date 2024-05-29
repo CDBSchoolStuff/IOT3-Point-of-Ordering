6b712f7f4742e346fa69582a2b3e555c52910591
HaXr <haxr@outlook.dk>
2024-05-29 17:52:17 +0200

    String to list handling

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
 
-import paho.mqtt.client as mqtt
-
-
 import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+from credentials_bar import credentials
 
 #########################################################################
-# CONFIGURATION
+# VARIABLES & CONSTANTS
 
 MQTT_TOPIC_BATTERY = "mqtt_bat"
 MQTT_TOPIC_ORDER = "mqtt_order"
 
+
+##########################################
+# FUNCTIONS
+
+
+
+
+##########################################
+# DATABASE STUFF
+
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user=credentials["mysql_user"],
+#   password=credentials['mysql_password']
+# )
+
+# mycursor = mydb.cursor()
+# try:
+#     mycursor.execute("CREATE DATABASE bar16") # Creates database named "bar16" if it doesn't already exist.
+# except:
+#     print("Database already exist. Continuing...")
+
+
+
+
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user=credentials["mysql_user"],
+#   password=credentials['mysql_password']
+#   database="bar16"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute(f"CREATE TABLE completed_orders (drink_type VARCHAR(255), amount VARCHAR(255)), table VARCHAR(255))")
 
 
 ##########################################
@@ -22,8 +58,6 @@ MQTT_TOPIC_ORDER = "mqtt_order"
 
 # https://pypi.org/project/paho-mqtt/
 import paho.mqtt.client as mqtt
-
-from credentials import credentials
 
 mqtt_server = credentials['mqtt_server']
 
@@ -40,13 +74,31 @@ try:
         client.subscribe(MQTT_TOPIC_ORDER)
         client.subscribe(MQTT_TOPIC_BATTERY)
 
+
     # The callback for when a PUBLISH message is received from the server.
     def on_message(client, userdata, msg):
         
         # Appends the inbound order to the orders list if message is received on order topic.
         if msg.topic == MQTT_TOPIC_ORDER:
-            order_data.append(msg.payload)
-            print(f"{msg.topic} {msg.payload}")
+            byte_string = msg.payload
+            decoded_string = byte_string.decode("utf-8")
+            
+            if len(decoded_string) > 50:
+                # initializing string
+                test_str = decoded_string
+                
+                # printing original string
+                print("The original string is : " + str(test_str))
+                
+                # eval() used to convert 
+                result = list(eval(test_str))
+                order_data.append(result)
+                print(f"{msg.topic} {result}")
+            
+            else:
+                result = eval(decoded_string)
+                order_data.append(result)
+                print(f"{msg.topic} {result}")
 
     mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     mqttc.on_connect = on_connect
@@ -73,9 +125,9 @@ except:
 def hello():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+@app.route('/statistics/')
+def statistics():
+    return render_template('statistics.html')
 
 @app.route('/orders/')
 def orders():
@@ -85,5 +137,19 @@ def orders():
     #             'This is the fourth order.'
     #             ]
     orders = order_data
+    
+    # print(len("{'name': 'Dark 'n Stormy', 'amount': 40}"))
+    # print(len("{'name': '0', 'amount': 0},{'name': '0', 'amount': 0}"))
+    # order_list = []
+    # for obj in order_data:
+    #     dict = eval(obj)
+    #     order_list.append(dict)
+
+    
+    # orders = order_list
+    
+    print(order_data)
+    #print(orders)
+    # amount = 
 
     return render_template('orders.html', orders=orders)
